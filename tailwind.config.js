425c98c521966e7aae3920cf028ac17ced204037
pakyardim <emirhanpakyardim@gmail.com>
2024-07-25 16:55:09 +0300

    chore(root): add fonts and absolute import configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Bangers", "sans-serif"],
+        secondary: ["Gabarito", "sans-serif"],
+      },
+      colors: {
+        primary: "#ff0000",
+        primaryDark: "#cc0000",
+        secondary: "#3b4cca",
+        tertiary: "#ffde00",
+        tertiaryDark: "#b3a125",
+        dark: "#333333",
+        darker: "#1e1e1e",
+        light: "#ffffff",
+        gray: "#807c7b",
+      },
+    },
   },
   plugins: [],
 };
